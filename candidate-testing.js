08b920726b25fc792c60c34dc81ad9a045c321dc
rcmcpherson <85424831+rcmcpherson@users.noreply.github.com>
2021-06-22 03:55:08 +0000

    Completed Part 2 of the assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,30 +8,39 @@ let candidateName = " ";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space?",
+  "True or false: 5 kilometer == 5000 meters?",
+  "(5 + 3)/2 * 10 = ?",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+  "What is the minimum crew size for the ISS?"
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName=input.question("What is your name? ");
-}
+candidateName = input.question("What is your name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+for (i=0; i<questions.length; i++) {
+  candidateAnswers[i] = input.question(questions[i]);
+};
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer == correctAnswer) {
-  console.log("That is correct!");
-} else {
-  console.log("Sorry. That was incorrect.");
-}
+console.log(`First question: You answered ${candidateAnswers[0]}, and the correct answer was ${correctAnswers[0]}.\nSecond question: You answered ${candidateAnswers[1]}, and the correct answer was ${correctAnswers[1]}.\nThird question: You answered ${candidateAnswers[2]}, and the correct answer was ${correctAnswers[2]}.\nFourth question: You answered ${candidateAnswers[3]}, and the correct answer is ${correctAnswers[3]}.\nFinal question: You answered ${candidateAnswers[4]}, and the correct answer is ${correctAnswers[4]}.`);
 
   let grade;
   
