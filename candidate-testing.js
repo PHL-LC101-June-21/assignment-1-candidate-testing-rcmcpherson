fe401b5abc9a3d7a1158f5832f6be85928548495
rcmcpherson <85424831+rcmcpherson@users.noreply.github.com>
2021-06-28 21:24:02 +0000

    Completed Part 2 of Graded Assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,11 +9,11 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 let questions = [
-  "Who was the first American woman in space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride",
@@ -28,7 +28,7 @@ let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("What is your name? ");
-
+}
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 for (i=0; i<questions.length; i++) {
@@ -40,11 +40,21 @@ for (i=0; i<questions.length; i++) {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log(`First question: You answered ${candidateAnswers[0]}, and the correct answer was ${correctAnswers[0]}.\nSecond question: You answered ${candidateAnswers[1]}, and the correct answer was ${correctAnswers[1]}.\nThird question: You answered ${candidateAnswers[2]}, and the correct answer was ${correctAnswers[2]}.\nFourth question: You answered ${candidateAnswers[3]}, and the correct answer is ${correctAnswers[3]}.\nFinal question: You answered ${candidateAnswers[4]}, and the correct answer is ${correctAnswers[4]}.`);
+console.log(`\nCandidate Name: ${candidateName}\n \nFirst question: \nYou answered: ${candidateAnswers[0]} \nThe correct answer is ${correctAnswers[0]}\n \nSecond question: \nYou answered: ${candidateAnswers[1]}\nThe correct answer is ${correctAnswers[1]}\n \nThird question: \nYou answered: ${candidateAnswers[2]}\nThe correct answer is ${correctAnswers[2]}\n \nFourth question: \nYou answered: ${candidateAnswers[3]}\nThe correct answer is ${correctAnswers[3]}\n \nFinal question: \nYou answered: ${candidateAnswers[4]}\nThe correct answer is ${correctAnswers[4]}\n `);
 
-  let grade;
-  
-
+  let grade="";
+  let correctNum=0
+  for (i=0;i<questions.length;i++){
+    if (correctAnswers[i].toLowerCase==candidateAnswers[i].toLowerCase){
+      correctNum++;
+    } 
+  }
+  grade=(correctNum/questions.length)*100;
+  if (grade>=80){
+    console.log(`Congratulations! You scored ${grade}% on the quiz. You passed!`);
+  } else {
+    console.log(`Sorry. You scored ${grade}% on the quiz. You need at least 80% to pass. You have failed.`);
+  }
   return grade;
 }
 
